eaa1da2895bebc60ec4c64ea94fb752146fe4fc8
Alen Varazdinac <alen.varazdinac@gmail.com>
2019-03-18 23:20:16 +0100

    Gallery folder

--- app/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'url' => 'http://localhost/projects/shared_gallery/',
-    'mode' => 'development',
-    'db_name' => 'shared-gallery',
-    'db_user' => 'root',
-    'db_password' => ''
-];
