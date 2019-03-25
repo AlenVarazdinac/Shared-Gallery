c06d1e7e94594e98f332f3ca05ee9f02d0be7b9f
Alen Varazdinac <alen.varazdinac@gmail.com>
2019-03-25 20:41:50 +0100

    Add config example

---
+++ app/config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+// Usage :
+// Delete '.example' from file name
+// and enter correct values in fields below
+
+return [
+    'url' => 'project_url',
+    'mode' => 'development',
+    'db_name' => 'db_name',
+    'db_user' => 'db_username',
+    'db_password' => 'db_password'
+];
